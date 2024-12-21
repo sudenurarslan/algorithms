5c785f611666bd50f2e38669184ecabcd40a0d0d
sudenurarslan <sudenurarslan13@gmail.com>
2024-12-21 17:50:37 +0300

    ReverseString algorithm is written

--- ReverseString.java
+++ ReverseString.java
@@ -1,4 +1,13 @@
 public class ReverseString {
-    
-    
+    public static void main(String[] args) {
+        String input = "Sude";
+        String reversed = reverseString(input);
+        System.out.println("Reversed string: " + reversed);
+    }
+
+    public static String reverseString(String str) {
+        StringBuilder reversedStr = new StringBuilder(str);
+        reversedStr.reverse();
+        return reversedStr.toString();
+    }
 }
