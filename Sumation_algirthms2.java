67a3ab9a96d807faf58b7cfb472bc78ed074c8fd
sudenurarslan <sudenurarslan13@gmail.com>
2024-12-21 18:17:39 +0300

    Sumation algorithm is added

---
+++ Sumation_algirthms2.java
@@ -0,0 +1,15 @@
+public class Sumation_algirthms2 {
+
+        public static void main(String[] args) {
+            int n = 10;
+            int sum = 0;
+    
+            for (int i = 1; i <= n; i++) {
+                sum += i;
+            }
+    
+            System.out.println("Sum of numbers from 1 to " + n + " is: " + sum);
+        }
+    }
+    
+
